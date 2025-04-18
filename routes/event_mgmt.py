bec56cea63ece9d23a6e70fe8d2cde435bc5357d
georgehynes <info@techybyte.co.uk>
2025-04-18 13:19:49 +0100

    confirm/reject agenda items, edit_agenda_item bug fixes and user role improvements

--- routes/event_mgmt.py
+++ routes/event_mgmt.py
@@ -1,9 +1,11 @@
+import logging as logger
+logging = logger.getLogger(__name__)
 from datetime import datetime, timedelta
 
 from flask import Blueprint, request, flash, redirect, url_for, render_template
 from flask_login import login_required, current_user
 
-from models import Event, db, AgendaItem, User, Course, AgendaItemStatus, EventNote, AgendaItemNote, Location
+from models import Event, db, AgendaItem, User, Course, AgendaItemStatus, EventNote, AgendaItemNote, Location, valid_user_roles
 from utils import check_permission
 
 bp = Blueprint('event_mgmt', __name__)
@@ -93,6 +95,7 @@ def archive_event_note(note_id):
 def edit_agenda_item(item_id):
     item = AgendaItem.query.get_or_404(item_id)
     lecturers = User.query.filter((User.role == 'lecturer') | (User.role == 'admin')).all()
+
     event = db.session.query(Event).get(item.event_id)
     course = db.session.query(Course).get(event.course_id)  # Retrieve the course
 
@@ -102,17 +105,35 @@ def edit_agenda_item(item_id):
     if request.method == 'POST':
         item.title = request.form['title']
         item.description = request.form['description']
-        if len(request.form['time']) > 5:
-            item.time = datetime.strptime(request.form['time'], '%H:%M:%S').time()
-        else:
-            item.time = datetime.strptime(request.form['time'], '%H:%M').time()
+        try:
+            if len(request.form['time']) > 5:
+                item.time = datetime.strptime(request.form['time'], '%H:%M:%S').time()
+            else:
+                item.time = datetime.strptime(request.form['time'], '%H:%M').time()
+        except ValueError:
+            logging.debug('Invalid time format')
 
         if len(request.form['duration']) > 0:
             item.duration = timedelta(minutes=int(request.form['duration']))
-        item.lecturer_id = request.form.get('lecturer_id') or None
-        if request.form.get('status') is not None:
-            item.status = request.form.get('status') if request.form.get(
-                'status') in AgendaItemStatus.__members__ else None
+
+        new_lecturer_id = request.form.get('lecturer_id')
+        if new_lecturer_id:
+            if new_lecturer_id == item.lecturer_id:
+                # No change in lecturer
+                pass
+            else:
+                # Change in lecturer
+                item.lecturer_id = new_lecturer_id
+                item.status = AgendaItemStatus.TENTATIVE
+        else:
+            # No lecturer assigned
+            item.lecturer_id = None
+            item.status = AgendaItemStatus.UNSCHEDULED
+
+        # if request.form.get('status') is not None:
+        #     item.status = request.form.get('status') if request.form.get(
+        #         'status') in AgendaItemStatus.__members__ else None
+        #
         db.session.commit()
         flash('Agenda item updated.')
         return redirect(url_for('event_mgmt.edit_event', event_id=item.event_id))
@@ -147,6 +168,29 @@ def agenda_item_notes(agenda_item_id):
 
     return render_template('agenda_item_notes.html', agenda_item=agenda_item, notes=agenda_item.filtered_notes,
                            can_add_note=can_add_note, can_archive_note=can_archive_note)
+
+
+@bp.route('/agenda_item/<int:item_id>/confirm', methods=['POST'])
+@login_required
+@check_permission('confirm_agenda_item')
+def confirm_agenda_item(item_id):
+    item = AgendaItem.query.get_or_404(item_id)
+    item.status = AgendaItemStatus.CONFIRMED
+    db.session.commit()
+    flash('Agenda item confirmed successfully.', 'success')
+    return redirect(url_for('event_mgmt.edit_agenda_item', item_id=item_id))
+
+
+@bp.route('/agenda_item/<int:item_id>/reject', methods=['POST'])
+@login_required
+@check_permission('reject_agenda_item')
+def reject_agenda_item(item_id):
+    item = AgendaItem.query.get_or_404(item_id)
+    item.status = AgendaItemStatus.UNSCHEDULED
+    item.lecturer_id = None
+    db.session.commit()
+    flash('Agenda item rejected successfully.', 'success')
+    return redirect(url_for('event_mgmt.edit_agenda_item', item_id=item_id))
 
 
 @bp.route('/agenda_item_note/<int:note_id>/archive', methods=['POST'])
