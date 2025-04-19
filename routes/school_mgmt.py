b321456b3dedec7b44c6ae4873f61ec06bcc3415
georgehynes <info@techybyte.co.uk>
2025-04-19 12:08:01 +0100

    Edit school and UI improvements

--- routes/school_mgmt.py
+++ routes/school_mgmt.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for, request, current_app
+from flask import Blueprint, render_template, redirect, url_for, request, current_app, flash
 from flask_login import login_required, current_user
 from sqlalchemy.orm import joinedload
 
-from models import db, School
+from models import db, School, User
 
 bp = Blueprint('school_mgmt', __name__)
 
@@ -31,9 +31,10 @@ def manage_schools():
         return redirect(url_for('school_mgmt.manage_schools'))  # Redirect to refresh list
 
     with current_app.app_context():
+        users = User.query.all()  # TODO: fetch only users that are not assigned to a school, ie. school_id is None
         schools = db.session.query(School).options(joinedload(School.programs)).all()
 
-    return render_template('schools.html', schools=schools)
+    return render_template('schools.html', schools=schools, users=users)
 
 
 @bp.route('/schools/<int:school_id>/delete', methods=['GET'])
@@ -52,4 +53,37 @@ def delete_school(school_id):
         db.session.delete(school)
         db.session.commit()
 
+    return redirect(url_for('school_mgmt.manage_schools'))
+
+
+@bp.route('/school/<int:school_id>/update', methods=['POST'])
+@login_required
+def update_school(school_id):
+    school = School.query.get_or_404(school_id)
+    school.name = request.form['name']
+    school.contact_name = request.form['contact_name']
+    school.contact_email = request.form['contact_email']
+    school.contact_phone = request.form['contact_phone']
+    db.session.commit()
+    flash('School details updated successfully.', 'success')
+    return redirect(url_for('school_mgmt.manage_schools'))
+
+
+@bp.route('/user/<int:user_id>/assign', methods=['POST'])
+@login_required
+def assign_user(user_id):
+    user = User.query.get_or_404(user_id)
+    user.school_id = request.form['school_id']
+    db.session.commit()
+    flash('User assigned to school successfully.', 'success')
+    return redirect(url_for('school_mgmt.manage_schools'))
+
+
+@bp.route('/user/<int:user_id>/unassign', methods=['POST'])
+@login_required
+def unassign_user(user_id):
+    user = User.query.get_or_404(user_id)
+    user.school_id = None
+    db.session.commit()
+    flash('User unassigned from school successfully.', 'success')
     return redirect(url_for('school_mgmt.manage_schools'))
